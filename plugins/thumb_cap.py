bb7fd29384e8e4325b884656dc00260db933e991
Kaizen <orewakaizensama@gmail.com>
2024-12-20 16:16:05 +0530

    Update thumb_cap.py

--- plugins/thumb_cap.py
+++ plugins/thumb_cap.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters, enums, Message
 from helper.database import db
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
