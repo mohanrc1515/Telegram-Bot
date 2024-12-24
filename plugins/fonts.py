e543f25f02c1cdb45e0bf874775ae17f6d7d9d90
Kaizen <orewakaizensama@gmail.com>
2024-12-24 16:30:45 +0530

    Update fonts.py

--- plugins/fonts.py
+++ plugins/fonts.py
@@ -1,5 +1,5 @@
 import os
-from plugins.helper.aks_font_func import Fonts
+from helper.aks_font_func import Fonts
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
