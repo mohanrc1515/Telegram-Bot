57fca0f65bf46735e92a356a6b4c1951d2126f72
Kaizen <orewakaizensama@gmail.com>
2025-01-31 10:33:39 +0530

    Update Cmd_&_cb.py

--- plugins/Cmd_&_cb.py
+++ plugins/Cmd_&_cb.py
@@ -27,6 +27,21 @@ async def admin_commands(client, message):
         ])
     )    
 
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    user = message.from_user
+    await db.add_user(client, message)                
+    button = InlineKeyboardMarkup([[
+            InlineKeyboardButton('Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/AutoRenameUpdates'),
+            InlineKeyboardButton('Sᴜᴩᴩᴏʀᴛ', url='https://t.me/Elites_Assistance')
+        ]])
+    if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)   
+        
+
+
 @Client.on_callback_query(filters.regex('start'))
 async def start_callback(client, query: CallbackQuery):
     await query.message.edit_text(
