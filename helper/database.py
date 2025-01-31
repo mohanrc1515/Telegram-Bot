e6c39c83b3cf7f428c99fc159de439e1c979eb5e
Kaizen <orewakaizensama@gmail.com>
2025-01-31 09:51:12 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -14,21 +14,18 @@ class Database:
         
     
     
-    # User handling
     def new_user(self, id):
         return {
             "_id": int(id),
             "file_id": None,
 	}
 
-    async def add_user(self, client, message):
-        u = message.from_user
+    async def add_user(self, b, m):
+        u = m.from_user
         if not await self.is_user_exist(u.id):
             user = self.new_user(u.id)
-            await self.user_col.insert_one(user)
-            await send_log(client, u)
-        if not await self.metadata_exists(u.id):
-            await self.add_metadata(u.id)
+            await self.col.insert_one(user)            
+            await send_log(b, u)
       
       
     async def is_user_exist(self, id):
