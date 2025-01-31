65f7d7a6469f89c540678203f9cde1821b2c5b08
Kaizen <orewakaizensama@gmail.com>
2025-01-31 21:41:36 +0530

    Update fap.py

--- plugins/fap.py
+++ plugins/fap.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from datetime import datetime
 from helper.database import db
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from helper.utils import calculate_age
 from config import Config
 
