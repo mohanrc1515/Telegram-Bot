a355d1d58fe5066a32ab80bd63346f571670b7c4
Kaizen <orewakaizensama@gmail.com>
2025-01-31 13:00:56 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -3,9 +3,11 @@ import aiohttp
 from datetime import datetime, timedelta
 from pytz import timezone
 from config import Config, Txt
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import asyncio
 from pyrogram.errors import FloodWait
+from typing import Union
+from pyrogram import enums
 
 
 def humanbytes(size):
@@ -101,3 +103,33 @@ async def send_log(bot, user):
         except Exception as e:
             print(f"Error sending log message: {e}")
             
+def extract_user(message: Message) -> Union[int, str]:
+    """extracts the user from a message"""
+    # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
+    user_id = None
+    user_first_name = None
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        user_first_name = message.reply_to_message.from_user.first_name
+
+    elif len(message.command) > 1:
+        if (
+            len(message.entities) > 1 and
+            message.entities[1].type == enums.MessageEntityType.TEXT_MENTION
+        ):
+           
+            required_entity = message.entities[1]
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
+        else:
+            user_id = message.command[1]
+            # don't want to make a request -_-
+            user_first_name = user_id
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            pass
+    else:
+        user_id = message.from_user.id
+        user_first_name = message.from_user.first_name
+    return (user_id, user_first_name)
