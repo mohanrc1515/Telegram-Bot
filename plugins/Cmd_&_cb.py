3b721556c286148b06ec57d9dd648be91e008921
Kaizen <orewakaizensama@gmail.com>
2025-01-31 13:49:28 +0530

    Update Cmd_&_cb.py

--- plugins/Cmd_&_cb.py
+++ plugins/Cmd_&_cb.py
@@ -2,6 +2,7 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from helper.database import db
+from config import Config
 
 
 # 📌 Command: /start
